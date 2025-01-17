494c056e7f9e87fee71d243e83c761cd125983e4
testerdevelop173 <artemjuravlev232@gmail.com>
2025-01-17 15:48:30 +0300

    создан модуль логирования - отдельно, пока он не видит мой API

--- loging.go
+++ loging.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("dfdfdf")
+	log.SetFlags(log.Ldate | log.Ltime | log.LUTC | log.Lshortfile)
+
+	const logFilePath = "apiLog.log"
+	//file, _ := os.Create(logFilePath)
+	file, err := os.Create(logFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+
+	log.Println("loging in file")
+	file.Close()
+}
+
